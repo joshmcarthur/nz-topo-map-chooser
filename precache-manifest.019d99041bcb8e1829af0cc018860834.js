d64d41744bed5f77dab1ea217e2730d8604921e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-05 00:52:59 +0000

    Deploy joshmcarthur/nz-topo-map-chooser to github.com/joshmcarthur/nz-topo-map-chooser.git:gh-pages

---
+++ precache-manifest.019d99041bcb8e1829af0cc018860834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57c6aa172f5f21439709",
+    "url": "/nz-topo-map-chooser/static/js/runtime~main.6e8bacb9.js"
+  },
+  {
+    "revision": "390f26d8cf6d51069038",
+    "url": "/nz-topo-map-chooser/static/js/main.57d18151.chunk.js"
+  },
+  {
+    "revision": "3005fdc4ff837d44aaea",
+    "url": "/nz-topo-map-chooser/static/js/2.9829a5dd.chunk.js"
+  },
+  {
+    "revision": "390f26d8cf6d51069038",
+    "url": "/nz-topo-map-chooser/static/css/main.25892bfe.chunk.css"
+  },
+  {
+    "revision": "3005fdc4ff837d44aaea",
+    "url": "/nz-topo-map-chooser/static/css/2.43767a4b.chunk.css"
+  },
+  {
+    "revision": "18dadf9d23bbe20afd52ddf602798408",
+    "url": "/nz-topo-map-chooser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90ee5aa5083bdd5092646b158637664b.js"
+  "/nz-topo-map-chooser/precache-manifest.019d99041bcb8e1829af0cc018860834.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/nz-topo-map-chooser/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
